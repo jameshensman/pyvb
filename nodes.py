cf6acc8d6f4d2723992a0e5a26800bb4c4b4545e
James Hensman <james.hensman@gmail.com>
2009-11-26 10:45:13 +0100

    new file:   nodes.py new file:   tests.py

---
+++ nodes.py
@@ -0,0 +1,289 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009 James Hensman
+# Licensed under the Gnu General Public license, see COPYING
+import numpy as np
+
+class ConjugacyError(ValueError):
+    """This does very little at the moment"""
+    def __init__(self,message):
+	ValueError.__init__(self,message)
+	
+class node:
+    def __init__(self):
+	pass
+    def addChild(self,other):
+	self.children.append(other)
+    def __add__(self,other):
+	return Addition(self,other)
+    def __mul__(self,other):
+	return Multiplication(self,other)
+    
+class Gaussian(node):
+    def __init__(self,dim,pmu,pprec):
+	node.__init__(self)
+	self.shape = (dim,1)
+	
+	#Deal with prior mu parent (pmu)
+	if type(pmu)==np.ndarray:
+	    assert pmu.shape==self.shape,"Parent mean array has incorrect dimension"
+	    self.get_pmu = lambda :pmu
+	    self.get_pExxT = lambda : np.dot(pmu,pmu.T)
+	elif sum([isinstance(pmu,e) for e in [Gaussian, Addition, Multiplication]]):
+	    assert pmu.shape==self.shape,"Parent mean node has incorrect dimension"
+	    self.get_pmu = pmu.pass_down_Ex
+	    self.get_pExxT = pmu.pass_down_ExxT
+	    pmu.addChild(self)
+	else:
+	    raise ConjugacyError,'bad'
+	
+	#Deal with prior precision parent (pprec)
+	if type(pprec)==np.ndarray:
+	    assert pprec.shape == (self.shape[0],self.shape[0]),"Parent precision array has incorrect dimension"
+	    self.get_pprec = lambda :pprec
+	elif isinstance(pprec,Gamma):
+	    self.get_pprec = lambda : np.eye(self.shape[0])*pprec.get_Ex()
+	    pprec.addChild(self)
+	elif type(pprec)==list:
+	    assert len(pprec)==self.shape[0]
+	    raise NotImplementedError
+	    # TODO 
+	elif type(prec)==Wishart:
+	    raise NotImplementedError
+	    # TODO
+	else:
+	    raise ConjugacyError
+	
+	self.children = []
+	self.observed=False
+	
+	#randomly initialize solution...
+	self.qmu = np.random.randn(self.shape[0],1)
+	self.qprec = np.eye(self.shape[0])*np.random.rand()
+	
+    def observe(self,val):
+	assert val.shape == self.shape,"Bad shape for observation data"
+	self.observed=True
+	self.obs_value = val
+	self.obs_xxT = np.dot(val,val.T)
+	
+    def update(self):
+	if self.observed:
+	    return # don't update this node if it's an observed one
+	#get parent messages
+	pmu = self.get_pmu()
+	pprec = self.get_pprec()
+	#get Child messages
+	child_exs = [e.pass_up_ex(self) for e in self.children]
+	child_precs = [e.pass_up_prec(self) for e in self.children]
+	#here's the calculation
+	self.qprec = pprec + sum(child_precs) #that's it?
+	#weighted_exs = np.dot(pprec,pmu) + sum([np.dot(c,x) for x,c in zip(child_exs,child_precs)])
+	weighted_exs = np.dot(pprec,pmu) + sum(child_exs)
+	self.qmu = np.linalg.solve(self.qprec,weighted_exs)
+	
+    def pass_down_Ex(self):
+	if self.observed:
+	    return self.obs_value
+	else:
+	    return self.qmu
+	    
+    def pass_down_ExxT(self):
+	if self.observed:
+	    return self.obs_xxT
+	else:
+	    return np.dot(self.qmu,self.qmu.T) + np.linalg.inv(self.qprec)
+	
+    def pass_up_ex(self,requester):
+	if self.observed:
+	    return np.dot(self.get_pprec(),self.obs_value)
+	else:
+	    return np.dot(self.qprec,self.qmu)
+	    
+    def pass_up_prec(self,requester):
+	if self.observed:
+	    return self.get_pprec()
+	else:
+	    return self.qprec
+	
+class Addition(node):
+    def __init__(self,x1,x2):
+	node.__init__(self)
+	self.x1 = x1
+	self.x2 = x2
+	self.children = []
+	assert x1.shape == x2.shape, "Bad shapes for addition"
+	self.shape = x1.shape
+	if type(x1) == np.ndarray:
+	    self.get_x1 = lambda : x1
+	else:
+	    self.get_x1 = self.x1.pass_down_Ex
+	    x1.addChild(self)
+	if type(x2) == np.ndarray:
+	    self.get_x2 = lambda : x2
+	else:
+	    self.get_x2 = self.x2.pass_down_Ex
+	    x2.addChild(self)
+	    
+    def pass_up_ex(self,requester):
+	"""return the sum of the expected value of the child nodes, minus the expected value of the co-parent"""
+	sumMu = sum([e.pass_up_ex(self) for e in self.children])
+	sumC = sum([e.pass_up_prec(self) for e in self.children])
+	if requester is self.x1:
+	    return sumMu - np.dot(sumC,self.get_x2())
+	elif requester is self.x2:
+	    return sumMu - np.dot(sumC,self.get_x1())
+	
+    def pass_up_prec(self,requester):
+	#get prec from children to pass upwards
+	sumC = sum([e.pass_up_prec(self) for e in self.children])
+	return sumC
+	
+    def pass_down_Ex(self):
+	return self.get_x1()+self.get_x2()
+    
+    def pass_down_ExxT(self):
+	Ex1,Ex2 = self.get_x1() , self.get_x2()
+	return self.x1.pass_down_ExxT()+self.x2.pass_down_ExxT() + np.dot(Ex1,Ex2.T) + np.dot(Ex2,Ex1.T)
+	
+
+class Multiplication(node):
+    def __init__(self,x1,x2):
+	node.__init__(self)
+	m1,n1 = x1.shape
+	m2,n2 = x2.shape
+	assert n1 == m2, "incompatible multiplication dimensions"
+	assert n2 == 1, "right hand object must be a vector"
+	self.shape = (m1,n2)
+	self.x1 = x1
+	self.x2 = x2
+	self.children = []
+	if type(x1) == np.ndarray:
+	    self.get_x1 = lambda : x1
+	    self.get_x1x1T = lambda : np.dot(x1,x1.T)
+	else:
+	    self.get_x1 = x1.pass_down_Ex
+	    self.get_x1x1T = x1.pass_down_ExxT
+	    x1.addChild(self)
+	if type(x2) == np.ndarray:
+	    self.get_x2 = lambda : x2
+	    self.get_x2x2T = lambda : np.dot(x2,x2.T)
+	else:
+	    self.get_x2 = x2.pass_down_Ex
+	    self.get_x2x2T = x2.pass_down_ExxT
+	    x2.addChild(self)
+    
+    def pass_up_ex(self,requester):
+	""""""
+	sumMu = sum([e.pass_up_ex(self) for e in self.children])
+	sumC = sum([e.pass_up_prec(self) for e in self.children])
+	if requester is self.x1:
+	    if self.x1.shape[1] == 1:#lhs is column: therefore rhs is scalar: easy enough
+		return float(self.get_x2())*sumMu
+	    else:
+		raise NotImplementedError,"Objects with width (transposes, hstacks) not supported yet"
+	elif requester is self.x2:
+	    return  np.dot(self.get_x1().T,sumMu)
+	
+	
+    def pass_up_prec(self,requester):
+	"""get prec from children to pass upwards, modify it by the co-parent"""
+	sumC = sum([e.pass_up_prec(self) for e in self.children])
+	if requester is self.x1:
+	    if self.x1.shape[1]==1:# one column: easy enough
+		x2x2T = self.x2.pass_down_ExxT()# this must be scalar in this case?
+		return sumC*float(x2x2T)
+	    else:
+		raise NotImplementedError,"Objects with width (transposes, hstacks) not supported yet"
+	elif requester is self.x2:
+	    if self.x1.shape[1]==1:#left object has only one column...
+		x1x1T = self.get_x1x1T()
+		return np.trace(np.dot(x1x1T,sumC))
+	    else:
+		raise NotImplementedError,"Objects with width (transposes, hstacks) not supported yet"
+    
+    def pass_down_Ex(self):
+	return np.dot(self.get_x1() , self.get_x2())
+	
+    def pass_down_ExxT(self):
+	if self.x1.shape[1] == 1:#rhs is scalar: this is quite easy
+	    return self.get_x1x1T() * float(self.get_x2x2T())
+	else:
+	    raise NotImplementedError, "hstacks, transposes etc are not implememted yet"
+
+class hstack(node):
+    def __init__(self,parents):
+	node.__init__(self)
+	assert type(parents)==list
+	dims = [e.shape[0] for e in parents]
+	assert np.all(dims[0]==np.array(dims)),"dimensions incompatible"
+	self.parents = parents
+	self.shape = (dims[0],len(parents))
+	self.children = []
+    def get_Ex(self):
+	return np.hstack(e.get_Ex() for e in parents)
+    def get_Exxt(self):
+	return # TODO
+	
+	
+	    
+class Gamma:
+    """Gamma does not inherrit from node because it cannot be added, muliplied etc"""
+    def __init__(self,a0,b0):
+	self.a0 = a0
+	self.b0 = b0
+	self.children = []
+	self.update_a()#initialise q to correct value
+	self.qb = np.random.rand()#randomly initialise solution
+	
+    def addChild(self,child):
+	self.children.append(child)
+	self.update_a()#re-initialise q to correct value
+	
+    def update_a(self):
+	self.qa = self.a0
+	for child in self.children:
+	    self.qa += 0.5*child.shape[0]
+    
+    def update(self):
+	"""update only the b parameter..."""
+	self.qb = self.b0
+	for child in self.children:
+	    self.qb += 0.5*np.trace(child.pass_down_ExxT()) + 0.5*np.trace(child.get_pExxT()) - np.trace(np.dot(child.pass_down_Ex(),child.get_pmu().T))
+    
+    def get_Ex(self):
+	return self.qa/self.qb
+	
+class DiagonalGamma:
+	def __init__(self,dim,a0s,b0s):
+		self.dim = dim
+		assert a0s.size==self.dim
+		assert b0s.size==self.dim
+		self.a0s = a0s.flatten()
+		self.b0s = b0s.flatten()
+		self.children = []
+		self.update_a()#initialise q to correct value
+		self.qb = np.random.rand()#randomly initialise solution
+		
+	def addchild(self,child):
+		assert child.shape == (self.dim,1)
+		self.children.append(child)
+		self.update_a()
+	def update_a(self):
+		self.qa = self.a0
+		for child in self.children:
+			self.qa += 0.5
+	def update(self):
+		self.qb = self.qb0
+		for child in self.children:
+			self.qb += 0.5*np.diag(child.pass_down_ExxT()) + 0.5*np.diag(child.get_pExxT()) - np.diag(np.dot(child.pass_down_Ex(),child.get_pmu().T))
+		
+	def get_Ex(self):
+		return np.diag(self.qa/self.qb)
+	    
+	    
+	
+	
+	
+	
+	
+	
---
+++ tests.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009 James Hensman
+# Licensed under the Gnu General Public license, see COPYING
+import numpy as np
+import pylab
+import nodes
+
+def simple_mean_inference():
+    truemean = 7.2
+    trueprec = 10
+    N = 20
+    y = np.random.randn(N)*np.sqrt(1./trueprec) + truemean
+    mu = nodes.Gaussian(1,np.array([[0]]),np.array([[1e-3]]))
+    y_nodes = [nodes.Gaussian(1,mu,np.array([[trueprec]])) for i in range(N)]
+    for yy,n in zip(y,y_nodes):
+	n.observe(yy.reshape(1,1))
+    mu.update()
+    print mu.qmu, truemean
+	
+def scalar_addition():
+    A1 = nodes.Gaussian(1,np.array([[0]]),np.array([[0.01]]))
+    A2 = nodes.Gaussian(1,np.array([[0]]),np.array([[0.01]]))
+    A3 = nodes.Gaussian(1,np.array([[0]]),np.array([[0.01]]))
+    C = nodes.Gaussian(1,A1+A2+A3,np.array([[10]]))
+    
+    C.observe(np.array([[12]]))
+    
+    for i in range(5000):
+	A1.update()
+	A2.update()
+	A3.update()
+	print np.hstack((A1.qmu,A2.qmu,A3.qmu)).flatten()
+
+def vector_addition():
+    A1 = nodes.Gaussian(3,np.zeros((3,1)),np.eye(3)*0.01)
+    A2 = nodes.Gaussian(3,np.zeros((3,1)),np.eye(3)*0.01)
+    A3 = nodes.Gaussian(3,np.zeros((3,1)),np.eye(3)*0.01)
+    C = nodes.Gaussian(3,A1+A2+A3,np.eye(3)*10)
+    
+    C.observe(np.array([[12],[6],[3]]))
+    
+    for i in range(5000):
+	A1.update()
+	A2.update()
+	A3.update()
+	print np.hstack((A1.qmu,A2.qmu,A3.qmu)).flatten()
+	
+    print '\n',A1.qprec
+    
+def multiplication_of_observed():
+    A1 = nodes.Gaussian(1,np.zeros((1,1)),np.eye(1)*1.)
+    A2 = nodes.Gaussian(1,np.zeros((1,1)),np.eye(1)*1.)
+    A1.observe(np.array([[2]]))
+    A2.observe(np.array([[3]]))
+    C = nodes.Gaussian(1,A1*A2,np.eye(1)*0.01)
+    for i in range(5):
+	C.update()
+	print C.qmu
+    
+def multiplication_by_matrix():
+    q = 2
+    d = 2
+    W = np.random.randn(d,q)
+    A = nodes.Gaussian(q,np.zeros((q,1)),np.eye(q)*0.01)
+    m = nodes.Multiplication(W,A)
+    B = nodes.Gaussian(d,m,np.eye(d)*100)
+    B.observe(np.random.randn(d,1))
+    
+    for i in range(5):
+	A.update()
+	print A.qmu
+	
+    
+    
+	
+def simple_regression():
+    N = 200
+    x = np.linspace(-1,1,N).reshape(N,1)
+    atrue = 0.7
+    btrue = 0.3
+    prec_true = 50.#we aren;t going to infer this in this test.
+    y = atrue*x + btrue + np.random.randn(N,1)*np.sqrt(1./prec_true)
+    
+    #vreate variable nodes
+    B = nodes.Gaussian(1,np.array([[0]]),np.array([[1e-2]]))#Gaussian node with 'fixed' wide prior
+    A = nodes.Gaussian(1,np.array([[0]]),np.array([[1e-2]]))
+    noise = nodes.Gamma(1e-3,1e-3)
+    
+    #create (plate of) observation nodes
+    Ys = []
+    for xx in x:
+	m = nodes.Multiplication(xx.reshape(1,1),A)
+	Ys.append(nodes.Gaussian(1,m+B,noise))
+    for n,yy in zip(Ys,y):
+	n.observe(yy.reshape(1,1))
+	
+    #inference 
+    for i in range(10):
+	A.update()
+	B.update()
+	noise.update()
+	print np.hstack((A.qmu,B.qmu)).flatten(),noise.get_Ex(), atrue, btrue, prec_true
+	
+    #sample from posterior:
+    Nsamples = 100
+    Asamples = np.random.randn(Nsamples,1)*np.sqrt(1./A.qprec) + A.qmu[0]
+    Bsamples = np.random.randn(Nsamples,1)*np.sqrt(1./B.qprec) + B.qmu[0]
+    
+    #plotting
+    xxlin = np.linspace(-1.2,1.2,200)
+    pylab.figure()
+    for a,b in zip(Asamples,Bsamples):
+	pylab.plot(xxlin,xxlin*a+b,'b',alpha=0.2)
+    pylab.plot(x,y,'r.')
+    pylab.show()
+    
+	
+	
+def simple_PCA():
+    #Set up a PCA problem. Latent dim must be one for the moment...
+    N = 100
+    d = 8       
+    prec_true = 100.
+    Z_true = np.random.randn(N,1)
+    W_true = np.random.randn(d,1)
+    mu_true = np.random.randn(d,1)
+    X = np.dot(Z_true,W_true.T) + mu_true.T + np.random.randn(N,d)*np.sqrt(1./prec_true)
+    
+    #create nodes
+    noise = nodes.Gamma(1e-3,1e-3)
+    W = nodes.Gaussian(d,np.zeros((d,1)),np.eye(d)*0.001)
+    Mu = nodes.Gaussian(d,np.zeros((d,1)),np.eye(d)*0.001)
+    Zs = [nodes.Gaussian(1,np.zeros((1,1)),np.eye(1)) for i in range(N)]
+    mults = [nodes.Multiplication(W,z) for z in Zs]
+    Xs = [nodes.Gaussian(d,m+Mu,noise) for m in mults]
+    [n.observe(v.reshape(d,1)) for n,v in zip(Xs,X)]
+    
+    #inference
+    for i in range(10):
+	W.update()
+	[e.update() for e in Zs]
+	Mu.update()
+	noise.update()
+	print np.hstack((np.linalg.qr(W.qmu)[0],np.linalg.qr(W_true)[0]))
+    print '\n',noise.qa/noise.qb,prec_true
+    
+def mean_and_variance_inference():
+    N = 100
+    truemu = 1.23
+    true_prec = 5.
+    Xdata = np.random.randn(N,1)*np.sqrt(1./true_prec) + truemu
+    prec = nodes.Gamma(1e-3,1e-3)
+    mu = nodes.Gaussian(1,np.zeros((1,1)),np.array([[1e-3]]))
+    nodes = [nodes.Gaussian(1,mu,prec) for n in range(N)]
+    for n,x in zip(nodes,Xdata):
+	n.observe(x.reshape(1,1))
+    for i in range(10):
+	mu.update()
+	prec.update()
+	print float(mu.qmu),truemu
+	print float(prec.qa/prec.qb),true_prec
+	
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
