b9d8b7f7b3d932d1c9585b49cfd134f866afb273
James Hensman <james@iamspartacus.(none)>
2009-12-02 15:18:20 +0100

    First attempt at missing data ability in Gaussian node

--- src/pyvb/nodes/gaussian.py
+++ src/pyvb/nodes/gaussian.py
@@ -64,6 +64,7 @@ class Gaussian(Node):
 		self.precision_parent.addChild(self)
 		
 		self.observed=False
+		self.partially_observed=False
 		
 		#randomly initialize solution...
 		self.qmu = np.random.randn(self.shape[0],1)
@@ -71,12 +72,14 @@ class Gaussian(Node):
 		self.qcov = np.linalg.inv(self.qprec)
 	
 	def observe(self,val):
-		"""assigns an observation to the node
+		"""assigns an observation to the node.
 			
 		Arguments
 		----------
 		val : numpy.array
-			observation vector of the same dimension as the node
+			observation vector of the same dimension as the node.  
+			NaN parts val are treated as missing data
+			
 			
 		Notes
 		----------
@@ -84,9 +87,18 @@ class Gaussian(Node):
 		is set to True and is treated appropriately when updating etc.
 		"""
 		assert val.shape == self.shape,"Bad shape for observation data"
-		self.observed = True
-		self.obs_value = val
-		self.obs_xxT = np.dot(val,val.T)
+		
+		if np.isnan(val).all():#there are no actual data - it's an array of nans...
+			return
+		elif np.isnan(val).any():#some missing data
+			self.partially_observed = True
+			self.obs_value = val
+			self.obs_index = np.nonzero(1-np.isnan(val))[0]
+			self.missing_index = np.nonzero(np.isnan(val))[0]
+		else:#full observation
+			self.observed = True
+			self.obs_value = val
+			self.obs_xxT = np.dot(val,val.T)
 	
 	def update(self):
 		# don't update this node if it's an observed one
@@ -101,9 +113,23 @@ class Gaussian(Node):
 		# here's the calculation
 		self.qprec = pprec + sum(child_m1s) #that's it!
 		self.qcov = np.linalg.inv(self.qprec)
-		#weighted_exs = np.dot(pprec,pmu) + sum([np.dot(c,x) for x,c in zip(child_exs,child_precs)])
 		weighted_exs = np.dot(pprec,pmu) + sum(child_m2s)
 		self.qmu = np.dot(self.qcov,weighted_exs)
+		
+		if self.partially_observed:
+			#calculate the posterior probability of q given the observations.
+			#find mean, covariance or observed parts
+			mu_obs = self.qmu[self.obs_index,:]
+			cov_obs = self.qcov.take(self.obs_index,0).take(self.obs_index,1)
+			cov_obs_inv = np.linalg.inv(cov_obs)
+			#find covariance between observed data and all data:
+			cov_obs_all = self.qcov.take(self.obs_index,1)
+			#calculate marginals and set into qcov,qmu and qprec
+			self.qmu += np.dot(cov_obs_all,np.dot(cov_obs_inv,mu_obs-self.qmu[self.obs_index,:]))
+			self.qcov -= np.dot( cov_obs_all,np.dot(cov_obs_inv,cov_obs_all.T))
+			self.qprec = np.linalg.inv(self.qcov)
+			
+			
 	
 	def pass_down_Ex(self):
 		"""Returns the Expected value of this node"""
