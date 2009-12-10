921cc55bc24faf865232b2f124aa7098b0c29a80
James Hensman <james.hensman@gmail.com>
2009-12-10 09:33:53 +0100

    Added the network class to the module. Added a 'fetch nodes'  method to the network class modified:   src/pyvb/__init__.py modified:   src/pyvb/network.py

--- src/pyvb/__init__.py
+++ src/pyvb/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 import nodes
-from network import network
+from network import Network
--- src/pyvb/network.py
+++ src/pyvb/network.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-class network:
+from nodes import *
+class Network:
 	def __init__(self,nodes=[]):
 		self.nodes = []
 		[self.addnode(n) for n in nodes]
@@ -19,6 +20,29 @@ class network:
 		for i in range(niters):
 			for n in self.nodes:
 				n.update()
+	
+	def fetch_network(self):
+		"""find all of the nodes connected to the nodes in the network"""
+		N_starting_nodes = len(self.nodes)
+		
+		new_nodes = True
+		while new_nodes:
+			new_nodes = 0
+			for n in self.nodes:
+				if sum(isinstance(n,Gaussian), isinstance(n,Addition), isinstance(n,Multiplication)):
+					new_children = [e for e in n.children if not e in self.nodes]
+					new_parents = [e for e in [n.mean_parent, precision_parent] if not e in self.nodes]
+					new_nodes += len(new_children)+len(new_parents)
+					self.nodes.extend(new_children)
+					self.nodes.extend(new_parents)
+				if sum(isinstance(n,Gamma), isinstance(n,DiagonalGamma), isinstance(wishart)):
+					new_children = [e for e in n.children if not e in self.nodes]
+					new_nodes += len(new_children)
+					self.nodes.extend(new_children)
+					
+		print "Found "+str(len(self.nodes)-N_starting_nodes)+" new nodes."  
+				
+			
 				
 		
 		
