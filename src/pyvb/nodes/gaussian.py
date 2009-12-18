5a198f50ea22bcc776f66d54c8fbc5e1b2f21c81
James Hensman <james.hensman@gmail.com>
2009-12-18 15:27:47 +0100

    added Diagonal Gaussian class modified:   src/pyvb/nodes/gaussian.py

--- src/pyvb/nodes/gaussian.py
+++ src/pyvb/nodes/gaussian.py
@@ -169,6 +169,47 @@ class Gaussian(Node):
 		pp =  self.precision_parent.pass_down_Ex()
 		return (pp,np.dot(pp,self.qmu))
 		
-
+class DiagonalGaussian(Gaussian):
+	"""A class to represent a diagonal matrix, whose (diagonal) values are jointly Gaussian distributed
+	
+	Notes
+	----------
+	The qmu and qprec values are kept as for a normal Gaussian Class."""
+	def __init__(self,dim,pmu,pprec):
+		Gaussian.__init__(self,dim,pmu,pprec)
+		self.shape = (self.shape[0],self.shape[0])
+	def pass_down_Ex(self):
+		return np.diag(self.qmu[:,0])
+	def pass_down_ExxT(self):
+		return np.diag(self.qmu[:,0]*self.qmu[:,0] + np.diag(self.qcov)) 
+	def pass_down_ExTx(self):
+		return self.pass_down_ExxT()
+	def update(self):
+		# don't update this node if it's an observed one
+		if self.observed:
+			return
+		# get parent messages
+		pmu = self.mean_parent.pass_down_Ex()
+		pprec = self.precision_parent.pass_down_Ex()
+		# get Child messages
+		child_messages = [e.pass_up_m1_m2(self) for e in self.children]
+		
+		#The messages have 4 parts: m1,m2,b,bbT, where b is the co-parent of the child (by multiplication)
+		qprec = pprec + np.sum([e[0]*e[3] for e in child_messages],0) #that's elementwise multiplication, folks. NOT np.dot()
+		self.qcov = np.linalg.inv(qprec)
+		weighted_exs = np.dot(pprec,pmu) + np.sum([e[1]*e[2] for e in child_messages],0)
+		self.qmu = np.dot(self.qcov,weighted_exs)
+		
+		if self.partially_observed:
+			#calculate the posterior probability of q given the observations.
+			#find covariance of observed parts
+			cov_obs = self.qcov.take(self.obs_index,0).take(self.obs_index,1)
+			cov_obs_inv = np.linalg.inv(cov_obs)
+			#find covariance between observed data and all data:
+			cov_obs_all = self.qcov.take(self.obs_index,1)
+			#calculate marginals and set into qcov,qmu and qprec
+			self.qmu += np.dot(cov_obs_all,np.dot(cov_obs_inv,self.obs_value[self.obs_index,:]-self.qmu[self.obs_index,:]))
+			self.qcov -= np.dot( cov_obs_all,np.dot(cov_obs_inv,cov_obs_all.T))
+		
 		
 	    
