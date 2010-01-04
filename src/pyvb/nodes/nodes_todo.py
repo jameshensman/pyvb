a09f6c5ddfb39584f8aaa7cf1e747ef0b08a97fe
James Hensman <james@iamspartacus.(none)>
2010-01-04 18:43:51 +0100

    removed old code from hstack class

--- src/pyvb/nodes/nodes_todo.py
+++ src/pyvb/nodes/nodes_todo.py
@@ -6,7 +6,6 @@ import node
 from scipy import special #needed fro calculating lower bound in gamma, wishart
 
 class ConjugacyError(ValueError):
-	
 	def __init__(self,message):
 		ValueError.__init__(self,message)	
 	
@@ -52,24 +51,6 @@ class hstack(node.Node):
 		m2 += sum([m[1]*float(m[2][i]) for m in child_messages])# TODO Shouldn;t this all be in the Multiplication node?
 		m2 -= sum([sum([np.dot(m[0]*m[-1][i,j],self.parents[j].pass_down_Ex()) for j in range(self.shape[1]) if not i==j]) for m in child_messages])
 		return m1,m2
-	#def pass_up_m1(self,requester):
-		##get child m1 messages - each message should be a tuple - the m1 from the (true) child and the xxT from the co-parent
-		#Cm1s = [c.pass_up_m1(self) for c in self.children]
-		##requester index
-		#i = self.parents.index(requester)
-		#return sum([m*float(xxt[i,i]) for m,xxt in Cm1s])
-	#def pass_up_m2(self,requester):
-		##get child m1 messages - each message should be a tuple - the m1 from the child and the xxT from the co-parent
-		#Cm1s = [c.pass_up_m1(self) for c in self.children]
-		##get child m2 messages - each message should be a tuple - the m2 from the child and the Ex from the co-parent
-		#Cm2s = [c.pass_up_m2(self) for c in self.children]
-		#i = self.parents.index(requester)
-		#ret = np.zeros((self.shape[0],1))
-		#ret += sum([m2*float(cop[i]) for m2,cop in Cm2s])# TODO Shouldn;t this be in the Multiplication node?
-		#ret -= sum([sum([np.dot(m1*xxt[i,j],self.parents[j].pass_down_Ex()) for j in range(self.shape[1]) if not i==j]) for m1,xxt in Cm1s])
-		#return ret
-		#return  sum([m*float(cop[i]) for m,cop in Cm2s])\
-		#- sum([np.dot( Cm1sum,p.pass_down_Ex())*float(xxtsum[i,self.parents.index(p)]) for p in self.parents if not p==requester]) 
 		
 	
 class Transpose(node.Node):
