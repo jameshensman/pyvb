384786acca81f18df50bb12895c296297168d06c
James Hensman <james.hensman@gmail.com>
2010-01-28 12:09:27 +0100

    started using cholesky for stability modified:   src/pyvb/nodes/gaussian.py

--- src/pyvb/nodes/gaussian.py
+++ src/pyvb/nodes/gaussian.py
@@ -4,6 +4,7 @@
 from node import *
 from nodes_todo import *
 import numpy as np
+from scipy.linalg import cho_factor, cho_solve
 
 class Gaussian(Node):
 	""" A node to model a Gaussian random variable
@@ -114,7 +115,10 @@ class Gaussian(Node):
 		child_messages = [e.pass_up_m1_m2(self) for e in self.children]
 		# here's the calculation
 		qprec = pprec + np.sum([e[0] for e in child_messages],0) #that's it!
-		self.qcov = np.linalg.inv(qprec)
+		qprec_chol = cho_factor(qprec)
+		self.qcov = cho_solve(qprec_chol,np.eye(self.shape[0]))
+		self.q_ln_det = .5/np.log(np.prod(np.diag(qprec_chol[0])))
+
 		weighted_exs = np.dot(pprec,pmu) + np.sum([e[1] for e in child_messages],0)
 		self.qmu = np.dot(self.qcov,weighted_exs)
 		
@@ -140,7 +144,7 @@ class Gaussian(Node):
 			-2* np.dot(self.qmu,self.mean_parent.pass_down_Ex().T)) ) 
 		if not (self.observed or self.partially_observed):
 			#relative entropy from prior
-			ret -= -0.5*self.shape[0]*np.log(2*np.pi) - 0.5*np.log(np.linalg.det(self.qcov)) - 0.5*self.shape[0]
+			ret -= -0.5*self.shape[0]*np.log(2*np.pi) - 0.5*self.q_ln_det - 0.5*self.shape[0]
 		elif self.partially_observed:
 			#relative entropy of unobserved part
 			ret -= 0.5*len(self.missing_index)*np.log(2*np.pi) -0.5*np.log(np.linalg.det(self.qcov.take(self.missing_index,0).take(self.missing_index,1))) - 0.5*len(self.missing_index)
@@ -197,32 +201,6 @@ class DiagonalGaussian(Gaussian):
 		return np.diag(self.qmu[:,0]*self.qmu[:,0] + np.diag(self.qcov)) 
 	def pass_down_ExTx(self):
 		return self.pass_down_ExxT()
-	def update(self):
-		# don't update this node if it's an observed one
-		if self.observed:
-			return
-		# get parent messages
-		pmu = self.mean_parent.pass_down_Ex()
-		pprec = self.precision_parent.pass_down_Ex()
-		# get Child messages
-		child_messages = [e.pass_up_m1_m2(self) for e in self.children]
-		
-		#The messages have 4 parts: m1,m2,b,bbT, where b is the co-parent of the child (by multiplication)
-		qprec = pprec + np.sum([e[0]*e[3] for e in child_messages],0) #that's elementwise multiplication, folks. NOT np.dot()
-		self.qcov = np.linalg.inv(qprec)
-		weighted_exs = np.dot(pprec,pmu) + np.sum([e[1]*e[2] for e in child_messages],0)
-		self.qmu = np.dot(self.qcov,weighted_exs)
-		
-		if self.partially_observed:
-			#calculate the posterior probability of q given the observations.
-			#find covariance of observed parts
-			cov_obs = self.qcov.take(self.obs_index,0).take(self.obs_index,1)
-			cov_obs_inv = np.linalg.inv(cov_obs)
-			#find covariance between observed data and all data:
-			cov_obs_all = self.qcov.take(self.obs_index,1)
-			#calculate marginals and set into qcov,qmu and qprec
-			self.qmu += np.dot(cov_obs_all,np.dot(cov_obs_inv,self.obs_value[self.obs_index,:]-self.qmu[self.obs_index,:]))
-			self.qcov -= np.dot( cov_obs_all,np.dot(cov_obs_inv,cov_obs_all.T))
 		
 		
 	    
